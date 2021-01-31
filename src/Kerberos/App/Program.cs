521fb277d9962546c7dceb99345cf12624e29a7e
Hymeck <noonimf@gmail.com>
2021-01-31 03:58:10 +0300

    add constants, few base functions

---
+++ src/Kerberos/App/Program.cs
@@ -0,0 +1,21 @@
+﻿using Library;
+using static System.Console;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "0";
+            WriteLine(input);
+            
+            var normalizedInput = Kerberos.normalizeLength(input);
+            WriteLine(normalizedInput);
+            
+            var blocks = Kerberos.toBlocks(normalizedInput);
+            foreach (var block in blocks) 
+                WriteLine(block);
+        }
+    }
+}
--- src/Kerberos/Library/Library.fs
+++ src/Kerberos/Library/Library.fs
@@ -1,5 +1,34 @@
 ﻿namespace Library
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+open Library
+
+module Kerberos =
+    let rec normalizeLength (source: string): string =
+        if ((source.Length * Constants.charSize) % Constants.blockSize) = 0
+        then source
+        else normalizeLength (source + string Constants.normalizeChar)
+
+    let rec normalizeBinary (source: string): string =
+        if source.Length = Constants.charSize
+        then source
+        else normalizeBinary (string Constants.zero + source)
+
+    let toBinaryFormat (source: string): string =
+        let binaryMapper (character: char) =
+            normalizeBinary (Convert.ToString(int character, 2))
+
+        String.collect binaryMapper source
+
+    let toBlocks (normalizedSource: string): List<string> =
+        let blockCount =
+            normalizedSource.Length * Constants.charSize
+            / Constants.blockSize
+
+        let blockRange =
+            seq { for i in 0 .. blockCount - 1 -> i }
+
+        let chunkMapper chunkIndex =
+            normalizedSource.Substring(chunkIndex * Constants.charPerBlock, Constants.charPerBlock)
+
+        [ for block in (Seq.map chunkMapper blockRange) -> block |> toBinaryFormat ]
