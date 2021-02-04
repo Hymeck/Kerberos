f9917164aaae23481284eefca3a6e8ed8d9f130f
Hymeck <noonimf@gmail.com>
2021-02-04 10:46:28 +0300

    get started with domain

---
+++ src/Kerberos/Kerberos/Library.fs
@@ -0,0 +1,20 @@
+﻿namespace Kerberos
+
+open System
+
+module Core =
+    type UserIdentifier = { login: string; password: string }
+
+    type TGSIdentifier = { identifier: string }
+
+    // todo: keys for TGT?
+    type TGT =
+        { userIdentifier: UserIdentifier
+          tgsIdentifier: TGSIdentifier
+          at: DateTime
+          duration: TimeSpan }
+
+    // todo: keys for AR response?
+    type ASResponse = {tgt: TGT; }
+    
+    type Aut = {userIdentifier: UserIdentifier; at: DateTime}
