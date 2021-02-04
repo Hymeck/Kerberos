ce895dfc8fe4ea1380ac53ff45ef763753b82dc2
Hymeck <noonimf@gmail.com>
2021-02-04 14:00:54 +0300

    add embryos of Domain and Core

---
+++ src/Kerberos/Kerberos/Core.fs
@@ -0,0 +1,26 @@
+﻿namespace Domain
+
+open Kerberos.Domain
+
+module Core =
+    let encryptASResponse (asResponse: ASResponse) (clientSecretKey: string) (tgsSecretKey: string): ASResponse=
+        // todo: encrypt AS response attribute with client secket key
+        // todo: encrypt tgt with TGS secret key
+        {attribute = asResponse.attribute; tgt = asResponse.tgt}
+    
+    let encryptTGSRequest (tgsRequest: TGSRequest) (tgsSessionKey: string): TGSRequest =
+        // todo: encrypt user authenticator with TGS session key
+        {tgt = tgsRequest.tgt; attribute = tgsRequest.attribute; userAuthenticator = tgsRequest.userAuthenticator}
+    
+    let encryptTGSResponse (tgsResponse: TGSResponse) (tgsSessionKey: string) (serviceSecretKey: string): TGSResponse =
+        // todo: encrypt TGS response attribute with TGS session key
+        // todo: encrypt service ticket with service secret key
+        {attribute = tgsResponse.attribute; serviceTicket = tgsResponse.serviceTicket}
+    
+    let encryptServiceRequest (serviceRequest: ServiceRequest) (serviceSessionKey: string): ServiceRequest =
+        // todo: encrypt user authenticator with service session key
+        {serviceTicket = serviceRequest.serviceTicket; userAuthenticator = serviceRequest.userAuthenticator}
+    
+    let encryptServiceResponse (serviceResponse: ServiceResponse) (serviceSessionKey: string): ServiceResponse =
+        // todo: encrypt service attribute
+        {attribute = serviceResponse.attribute}
---
+++ src/Kerberos/Kerberos/Domain.fs
@@ -0,0 +1,70 @@
+﻿namespace Kerberos
+
+open System
+open System.Collections.Immutable
+open System.Net
+
+module Domain =
+    type ASRequest =
+        { id: string
+          serviceId: string
+          ipAddress: IPAddress
+          tgtLifetime: TimeSpan }
+
+    type TicketGrantingTicket =
+        { userId: string
+          tgsId: string
+          timestamp: DateTime
+          userIpAddress: ImmutableList<IPAddress>
+          lifetime: TimeSpan
+          tgsSessionKey: string }
+
+    type ASResponseAttribute =
+        { tgsId: string
+          timestamp: DateTime
+          lifetime: TimeSpan (* same as TGT lifetime *)
+          tgsSessionKey: string }
+
+    type ASResponse =
+        { attribute: ASResponseAttribute (* encrypt with client secket key *)
+          tgt: TicketGrantingTicket (* encrypt with TGS secret key *)  }
+
+    type TGSRequestAttribute =
+        { serviceId: string
+          ticketLifetime: TimeSpan }
+
+    type UserAuthenticator = { userId: string; timestamp: DateTime }
+
+    type TGSRequest =
+        { tgt: TicketGrantingTicket (* was encrypted with TGS secret key *)
+          attribute: TGSRequestAttribute
+          userAuthenticator: UserAuthenticator (* encrypt with TGS session key *)  }
+
+    type TGSResponseAttribute =
+        { serviceId: string
+          timestamp: DateTime
+          lifetime: TimeSpan
+          serviceSessionKey: string }
+
+    type ServiceTicket =
+        { userId: string
+          serviceId: string
+          timestamp: DateTime
+          userIpAddress: ImmutableList<IPAddress>
+          serviceTicketLifetime: TimeSpan
+          serviceSessionKey: string }
+
+    type TGSResponse =
+        { attribute: TGSResponseAttribute (* encrypt with TGS session key *)
+          serviceTicket: ServiceTicket (* encrypt with service secret key *)  }
+
+    type ServiceRequest =
+        { serviceTicket: ServiceTicket (* was encrypted with service secret key *)
+          userAuthenticator: UserAuthenticator (* encypt with service session key *)  }
+
+    type ServiceAttribute =
+        { serviceId: string
+          timestamp: DateTime }
+
+    type ServiceResponse =
+        { attribute: ServiceAttribute (* encypt with service session key *)  } 
