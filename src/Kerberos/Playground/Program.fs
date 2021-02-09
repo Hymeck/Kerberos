34656b2d91f56c6fd0dd3ef74e2824853cccbc4e
Hymeck <noonimf@gmail.com>
2021-02-09 21:14:24 +0300

    add notes to demo flow

--- src/Kerberos/Playground/Program.fs
+++ src/Kerberos/Playground/Program.fs
@@ -13,8 +13,16 @@ let ip = IPAddress.Parse("127.0.0.1")
 let tgtLifetime = TimeSpan.FromSeconds(float 10)
 
 let printObject (value: obj) =
-    Console.WriteLine()
     Console.WriteLine(value)
+
+let printInfo (info: string) =
+    Console.WriteLine(info)
+
+let printInfoWithObject (info: string) (value: obj) =
+    Console.WriteLine("---")
+    printInfo info
+    printObject value
+    Console.WriteLine("---")
     Console.WriteLine()
 
 [<EntryPoint>]
@@ -27,50 +35,48 @@ let main argv =
 
     let asRequest = createASRequest userData
 
-    printObject asRequest
+    printInfoWithObject "1. Request to AS:" asRequest
     let asResponse = sendASRequest asRequest
 
     match asResponse with
-    | Some r -> printObject r
-    | None -> Console.WriteLine("User does not exist")
+    | Some r -> printInfoWithObject "2. AS response:" r
+    | None -> Console.WriteLine("User does not exist.")
 
     let decodedAsResponse =
         decryptASResponse asResponse.Value (userSecret password)
 
-    printObject decodedAsResponse
+    printInfoWithObject "Decrypted AS value using user secret key: " decodedAsResponse
 
     let tgsRequest =
         createTGSRequest asResponse.Value.tgt serviceId tgtLifetime userId decodedAsResponse.tgsSessionKey
 
-    printObject tgsRequest
+    printInfoWithObject "3. Request to TGS:" tgsRequest
 
     let tgsResponse = sendTGSRequest tgsRequest
 
     match tgsResponse with
-    | Some r -> printObject r
-    | None -> Console.WriteLine("Something goes wrong with TGS")
+    | Some r -> printInfoWithObject "4. TGS response:" r
+    | None -> Console.WriteLine("Something goes wrong with TGS.")
 
     let decodedTgsResponse =
         decryptTGSResponse tgsResponse.Value decodedAsResponse.tgsSessionKey
 
-    printObject decodedTgsResponse
+    printInfoWithObject "Decrypted TGS value using TGS session key:" decodedTgsResponse
 
     let serviceRequest =
         createServiceRequest tgsResponse.Value.serviceTicket userId decodedTgsResponse.serviceSessionKey
 
-    printObject serviceRequest
+    printInfoWithObject "5. Service request:" serviceRequest
 
     let serviceResponse =
         sendServiceRequest serviceRequest serviceId
 
     match serviceResponse with
-    | Some r -> printObject r
-    | None -> Console.WriteLine("Something goes wrong with Service")
-
-    printObject serviceResponse.Value
+    | Some r -> printInfoWithObject "6. Service response:" r
+    | None -> Console.WriteLine("Something goes wrong with Service.")
 
     let decodedServiceResponse =
         decryptServiceResponse serviceResponse.Value decodedTgsResponse.serviceSessionKey
 
-    printObject decodedServiceResponse
+    printInfoWithObject "Decrypted Service value using service session key:" decodedServiceResponse
     0
